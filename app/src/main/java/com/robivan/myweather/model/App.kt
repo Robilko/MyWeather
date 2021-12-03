ce0f99a5819b83f23f7ed8b663c7c001467273b1
Robilko <sasha.robson@gmail.com>
2021-12-03 18:20:53 +0300

    Урок 11. Добавлены push-нотификации

--- app/src/main/java/com/robivan/myweather/model/App.kt
+++ app/src/main/java/com/robivan/myweather/model/App.kt
@@ -2,7 +2,9 @@ package com.robivan.myweather.model
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 import androidx.room.Room
+import com.google.firebase.messaging.FirebaseMessaging
 import com.robivan.myweather.room.HistoryDao
 import com.robivan.myweather.room.HistoryDataBase
 import java.lang.IllegalStateException
@@ -12,6 +14,12 @@ class App : Application() {
         super.onCreate()
         appInstance = this
         appContext = applicationContext
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d("MyFMessagingService", "token = ${task.result.toString()}")
+            }
+        }
     }
 
     companion object {
